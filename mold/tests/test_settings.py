da1dc96f28491250c098abe107636f3eee5111cc
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-06-23 16:54:21 +0530

    Update : settings unit test case :construction_worker: :white_check_mark:

--- mold/tests/test_settings.py
+++ mold/tests/test_settings.py
@@ -12,6 +12,9 @@
 # future
 from __future__ import unicode_literals
 
+# third party
+import os
+
 # Django
 from django.conf import settings
 from django.test import TestCase
@@ -22,21 +25,29 @@ class SettingsTestCase(TestCase):
 
     """
 
+    pass
+
+
+class EnvironmentVariableTestCase(TestCase):
+    """Environment Variables Test case
+    """
     def setUp(self):
         """
-
         """
-        self.env_settings = ('SECRET_KEY', )
+        self.env_variables = (
+            'DATABASE_NAME_TEMPLATE',
+            'DATABASE_USER',
+            'DATABASE_PASSWORD',
+            'DATABASE_HOST',
+            'DATABASE_PORT',
+            'SECRET_KEY',
+        )
 
-    def test_environment_variables(self):
-        """makes sure settings which are necessary for running Member micro-service are defined in settings.
-
+    def test_env_variables(self):
+        """Makes sure necessary env variables are declared.
         """
-
-        for env_setting in self.env_settings:
-            if not getattr(settings, env_setting,):
-                self.assertFalse('{0} environment settings is not defined.'.format(env_setting))
-
-        self.assertTrue('Environment settings test passed.')
-
-
+        for key in self.env_variables:
+            try:
+                return os.environ[key]
+            except KeyError:
+                self.assertFalse('{0} environment variable is not defined.'.format(key))
