91ab76e31cba0f004d7d8e4e6d19f66577335856
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-06-26 19:06:26 +0530

    Upadte : ststic files path :construction_worker:

--- config/base.py
+++ config/base.py
@@ -138,7 +138,7 @@ MEDIA_URL = '/media/'
 
 # ######### STATIC FILE CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = normpath(join(SITE_ROOT, 'assets'))
+STATIC_ROOT = normpath(join(SITE_ROOT, 'static'))
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
