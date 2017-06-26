e56107d2d0ef9ae10b392928b5bead0b65656342
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-06-26 20:06:10 +0530

    refactor : gunicorn dependency removed :fire:

--- config/docker/docker-entrypoint.sh
+++ config/docker/docker-entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Prepare log files and start outputting logs to stdout
-touch /var/log/gunicorn.log
-touch /var/log/access.log
-tail -n 0 -f /var/log/*.log &
+#touch /var/log/gunicorn.log
+#touch /var/log/access.log
+#tail -n 0 -f /var/log/*.log &
 
 # Start Gunicorn processes
 #echo Starting Gunicorn.
-exec gunicorn config.wsgi:application \
-    --name v-templates \
-    --bind 0.0.0.0:80 \
-    --workers 3 \
-    --log-level=info \
-    "$@"
+#exec gunicorn config.wsgi:application \
+#    --name v-templates \
+#    --bind 0.0.0.0:80 \
+#    --workers 3 \
+#    --log-level=info \
+#    "$@"
+exec python manage.py runserver 0.0.0.0:80
