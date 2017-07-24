bd5a8450518c231aa300b5b00cf76e55822c326f
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-07-24 16:06:16 +0530

    Refactor : DRF relate dsettings removed :fire: controlled from base file

--- config/production.py
+++ config/production.py
@@ -96,33 +96,6 @@ CACHES = {
 }
 ########## END CACHE CONFIGURATION
 
-
-########## DJANGO REST FRAMEWORK CONFIGURATION
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
-        'rest_framework_swagger.renderers.OpenAPIRenderer',
-        'rest_framework_swagger.renderers.SwaggerUIRenderer',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ),
-    'DEFAULT_FILTER_BACKENDS': (
-        'rest_framework.filters.OrderingFilter',
-        'rest_framework.filters.SearchFilter'
-    ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 50
-}
-
 REST_FRAMEWORK_EXTENSIONS = {
     'DEFAULT_CACHE_RESPONSE_TIMEOUT': 30
 }
